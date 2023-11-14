bf2f13c2a5ea3ba009b77c82fa7fd566117d5e23
JMCA2805 <josemauricio2805@gmail.com>
2023-11-14 01:37:10 -0400

    Acomodando el responive y estilos de Hero, Resumen y Services

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -4,7 +4,7 @@ export default class Hero extends Component {
   render() {
     return (
         <section className="bg-[url('/exterior-oscuro.png')] bg-cover bg-center dark:bg-gray-900 font-poppins bg-fixed sm:bg-center">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             {/* Contenido principal */}
             <div className="mr-auto place-self-center lg:col-span-7">
                 {/* Título */}
@@ -13,8 +13,8 @@ export default class Hero extends Component {
                 <p className="max-w-2xl mb-6 font-light text-white lg:mb-8 md:text-lg lg:text-1xl dark:text-white">Te invitamos a descubrir la hospitalidad y el encanto de nuestro hotel. ¡Reserva ahora y déjanos hacer de tu estadía una experiencia inolvidable!.</p>
                 {/* Botones */}
 
-                <a href="#" className=" text-white bg-Moradote inline-flex items-center justify-center hover:bg-MoradoO px-5 py-3 text-base font-medium text-center  border border-gray-300 rounded-lg hover:bg-rojo hover:text-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                    Inicio
+                <a href="#" className="inline-block px-3 py-2 rounded-md text-white font-bold bg-Moradote focus:outline-none focus:text-white hover:border-2 hover:border-VerdeO ">
+                    Reservar
                 </a> 
             </div>  
             {/* Imagen */}  
--- src/components/Landing/Resumen.jsx
+++ src/components/Landing/Resumen.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 function Resumen() {
   const slides = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
     },
     {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
     },
     {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
     },
     {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
     },
   ];
 
@@ -40,38 +40,45 @@ function Resumen() {
   };
 
   return (
-    <div className='flex max-w h-[780px] m-auto py-16 font-poppins bg-MoradoO mx-4 my-4 text-white rounded-2xl '>
-      <div className='w-1/2'>
+    <div className="flex flex-col md:flex-row max-w h-[800px] md:h-[500px] md:px-4 lg:px-8 m-auto py-16 font-poppins bg-MoradoO mx-4 my-4 text-white rounded-2xl gap-8 md:gap-0">
+      <div className="w-full md:w-1/2 px-8 md:px-0 h-1/2 md:h-full">
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className='w-full ml-4 h-full rounded-2xl bg-center bg-cover duration-500'
+          className="w-full md:ml-4 ml-0 h-full rounded-2xl bg-center bg-cover duration-500"
         ></div>
         {/* Left Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
         {/* Right Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
-        <div className='flex top-4 justify-center py-2'>
+        <div className="flex top-4 justify-center py-2">
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
-              className='text-2xl cursor-pointer'
+              className="text-2xl cursor-pointer"
             >
               <RxDotFilled />
             </div>
           ))}
         </div>
       </div>
-      <div className='w-1/2 pl-16 mr-6'>
-        <h1 className='text-4xl font-bold mb-4'>Resumen</h1>
-        <p className='text-lg'>
-        El Hotel Águila era un lugar emblemático en Caracas, Venezuela, conocido por su elegancia y servicio impecable. 
-        Fundado hace más de cien años, el hotel era famoso por su belleza y comodidades, así como por su historia como refugio para disidentes políticos durante la dictadura. A pesar de la crisis económica, el hotel seguía siendo un lugar especial para muchos caraqueños, que recordaban con cariño los momentos felices que habían vivido entre sus paredes. 
-        Aunque su futuro era incierto, siempre permanecería en la memoria de quienes lo conocieron en su época dorada.        </p>
+      <div className="w-full h-1/2 md:h-full md:w-1/2 p-8 md:p-0 mr-0 md:px-0 md:pl-16 md:mr-6 ">
+        <h1 className="lg:text-4xl font-bold mb-4 sm:text-3xl text-2xl">Resumen</h1>
+        <p className="lg:text-lg  min-[900px]:text-md min-[400px]:text-sm min-[300px]:text-xs">
+          El Hotel Águila era un lugar emblemático en Caracas, Venezuela,
+          conocido por su elegancia y servicio impecable. Fundado hace más de
+          cien años, el hotel era famoso por su belleza y comodidades, así como
+          por su historia como refugio para disidentes políticos durante la
+          dictadura. A pesar de la crisis económica, el hotel seguía siendo un
+          lugar especial para muchos caraqueños, que recordaban con cariño los
+          momentos felices que habían vivido entre sus paredes. Aunque su futuro
+          era incierto, siempre permanecería en la memoria de quienes lo
+          conocieron en su época dorada.{" "}
+        </p>
       </div>
     </div>
   );
