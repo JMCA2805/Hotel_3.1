8880a97a878a76dba4c014f356fe6a8eeb875019
Luis Monsalve <126512492+Luismonsalver@users.noreply.github.com>
2023-11-15 19:55:01 -0400

    Update/ TablaUsers
    Actualizando los estilos de la tabla de usuarios

--- src/components/Admin/TablaUsers.jsx
+++ src/components/Admin/TablaUsers.jsx
@@ -72,85 +72,107 @@ const UserTable = () => {
   };
 
   return (
-    <div className="overflow-x-auto font-[Barlow] mb-8">
-              <div className="bg-MoradoC dark:bg-MoradoO rounded-lg p-4 mx-4 mt-4 sm:mx-28 mb-8">
-          <h2 className="text-white text-3xl font-bold text-center">Lista de Usuarios</h2>
-        </div>
+    <div className="font-[Barlow] mb-8">
+      <div className="bg-MoradoC dark:bg-MoradoO rounded-lg p-4 mx-4 mt-4 sm:mx-28 mb-8 border dark:border-VerdeC border-MoradoO">
+        <h2 className="text-white text-3xl font-bold text-center">
+          Lista de Usuarios
+        </h2>
+      </div>
       {users.length === 0 ? (
-        <p>No hay reservas disponibles.</p>
+        <div className="flex w-full justify-center items-center text-white">
+          <p>No hay usuarios disponibles.</p>
+        </div>
       ) : (
-        <table className="sm:text-xs md:text-sm text-left text-white dark:text-white mb-6  mx-10">
-          <thead className="text-white bg-MoradoC dark:bg-MoradoO dark:text-white">
-            <tr className="text-center">
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Nombre
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Apellido
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Correo
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Contraseña
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Descripción
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Teléfono
-              </th>
-              <th scope="col" className="sm:p-2 md:px-6 md:py-3 text-center">
-                Acciones
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array.isArray(users) ? (
-              users.map((usuario) => (
-                <tr key={usuario.id}>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.nombre}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.apellido}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.correo}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.contraseña}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.descripcion}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    {usuario.telefono}
-                  </td>
-                  <td className="sm:p-2 md:px-6 md:py-3 text-center">
-                    <div className="flex items-center">
-                      <button
-                        className="text-white bg-verdeo hover:bg-blue-800 px-4 py-2 rounded-lg mr-2"
-                        onClick={() => editarUsuario(usuario)}
-                      >
-                        Editar
-                      </button>
-                      {/* Otros botones de acción */}
-                    </div>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan="7">Cargando usuarios...</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+        <div className="w-full px-10 h-full">
+          <div className="overflow-x-auto p-8 w-full h-full rounded-xl dark:bg-MoradoO/50 bg-Moradote/50">
+            <div className="inline-block min-w-full shadow rounded-xl overflow-hidden h-full dark:border-VerdeC border border-MoradoO">
+              <table className="min-w-full leading-normal  sm:text-xs md:text-sm text-left text-white dark:text-white mb-6">
+                <thead className="text-white bg-MoradoC dark:bg-MoradoO dark:text-white border-b dark:border-VerdeC border-MoradoO">
+                  <tr className="text-center">
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Nombre
+                    </th>
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Apellido
+                    </th>
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Correo
+                    </th>
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Descripción
+                    </th>
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Teléfono
+                    </th>
+                    <th
+                      scope="col"
+                      className="sm:p-2 md:px-6 md:py-3 text-center"
+                    >
+                      Acciones
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.isArray(users) ? (
+                    users.map((usuario) => (
+                      <tr key={usuario.id}>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          {usuario.nombre}
+                        </td>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          {usuario.apellido}
+                        </td>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          {usuario.correo}
+                        </td>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          {usuario.descripcion}
+                        </td>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          {usuario.telefono}
+                        </td>
+                        <td className="sm:p-2 md:px-6 md:py-3 text-center">
+                          <div className="flex items-center w-full">
+                            <button
+                              className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
+                              onClick={() => editarUsuario(usuario)}
+                            >
+                              Editar
+                            </button>
+                            {/* Otros botones de acción */}
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="7">Cargando usuarios...</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       )}
 
       {usuarioSeleccionado && (
-        <div className="mx-24 font-[Barlow]">
+        <div className="mx-24 font-[Barlow] bg-MoradoC dark:bg-MoradoO rounded-lg p-4 text-white mt-8 border dark:border-VerdeC border-MoradoO">
           <h2 className="text-lg font-semibold">Editar usuario</h2>
           <form>
             <div className="flex flex-col mt-2">
@@ -161,7 +183,7 @@ const UserTable = () => {
                 name="nombre"
                 value={datosActualizados.nombre || ""}
                 onChange={handleInputChange}
-                className="border border-gray-300 p-2 rounded-md"
+                className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
               />
             </div>
             <div className="flex flex-col mt-2">
@@ -172,7 +194,7 @@ const UserTable = () => {
                 name="apellido"
                 value={datosActualizados.apellido || ""}
                 onChange={handleInputChange}
-                className="border border-gray-300 p-2 rounded-md"
+                className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
               />
             </div>
             <div className="flex flex-col mt-2">
@@ -183,7 +205,7 @@ const UserTable = () => {
                 name="descripcion"
                 value={datosActualizados.descripcion || ""}
                 onChange={handleInputChange}
-                className="border border-gray-300 p-2 rounded-md"
+                className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
               />
             </div>
             <div className="flex flex-col mt-2">
@@ -194,16 +216,27 @@ const UserTable = () => {
                 name="telefono"
                 value={datosActualizados.telefono || ""}
                 onChange={handleInputChange}
-                className="border border-gray-300 p-2 rounded-md"
+                className="w-full p-2 border border-verdeo rounded-md bg-MoradoO/30 border-MoradoO text-white placeholder:text-white/50 focus:border-2 focus:border-MoradoO focus:ring-0 dark:border-VerdeC/50 dark:focus:border-VerdeC"
               />
             </div>
             {/* Otros campos del formulario */}
-            <button
-              className="bg-verdeo hover:bg-verdeo-dark text-white font-bold py-2 px-4 rounded mt-4"
-              onClick={actualizarUsuario}
-            >
-              Guardar
-            </button>
+            <div className="flex mt-4">
+              <button
+                type="submit"
+                className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
+                onClick={() => {
+                  setUsuarioSeleccionado(null);
+                }}
+              >
+                Cancelar
+              </button>
+              <button
+                className="text-white px-4 py-2 rounded-lg mr-2 bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-Moradote/50 dark:hover:bg-MoradoC/70 focus-within:bg-MoradoO"
+                onClick={actualizarUsuario}
+              >
+                Guardar
+              </button>
+            </div>
           </form>
         </div>
       )}
