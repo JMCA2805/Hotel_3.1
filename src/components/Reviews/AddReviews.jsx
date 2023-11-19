83827ca07371c5fc15ac19fa116c0ce2f4544357
JMCA2805 <josemauricio2805@gmail.com>
2023-11-19 01:36:32 -0400

    Modal para agregar reseñas parte 2

--- src/components/Reviews/AddReviews.jsx
+++ src/components/Reviews/AddReviews.jsx
@@ -1,15 +1,51 @@
 import { Modal } from "flowbite-react";
+import { AuthContext } from "../../context/AuthProvider";
+import { useContext, useState } from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
+
+const API = import.meta.env.VITE_REVIEWS_URL;
 
 export default function AddReviews({ openModal, handleModalSet }) {
+  const [comentario, setComentario] = useState("");
+  const { user } = useContext(AuthContext);
 
-  const Añadir = ()=>{
+  const Añadir = async (e) => {
+    e.preventDefault();
 
-  }
-  
+    try {
+      const response = await axios.post(API, {
+        nombre: user.nombre,
+        comentario: comentario,
+      });
+      Swal.fire({
+        icon: "success",
+        title: "Reseña Enviada",
+        text: "¡Reseña enviada con exito!",
+        confirmButtonText: "Aceptar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          handleModalSet();
+          setComentario("");
+        } else {
+          handleModalSet();
+          setComentario("");
+        }
+      });
+    } catch (error) {
+      console.error("Error al enviar la reseña:", error);
+
+      Swal.fire({
+        icon: "error",
+        title: "Error al enviar la reseña",
+        text: "Porfavor intente nuevamente",
+      });
+    }
+  };
+
   return (
     <>
       <Modal
-        dismissible
         show={openModal}
         onClose={handleModalSet}
         position={"center"}
@@ -28,6 +64,9 @@ export default function AddReviews({ openModal, handleModalSet }) {
             </div>
             <textarea
               name="Comentarios"
+              onChange={(e) => {
+                setComentario(e.target.value);
+              }}
               className="bg-MoradoO/30 border border-MoradoO text-white placeholder:text-white/50 sm:text-sm rounded-lg focus:border-2 focus:border-MoradoO focus:ring-0 block w-full p-4 dark:border-VerdeC/50 dark:focus:border-VerdeC h-60"
             />
           </div>
@@ -43,7 +82,10 @@ export default function AddReviews({ openModal, handleModalSet }) {
                 </div>
               </button>
               <button
-                onClick={handleModalSet}
+                onClick={() => {
+                  handleModalSet;
+                  setComentario("");
+                }}
                 className="inline-block rounded-md bg-Moradote focus:outline-none focus:text-white border-b-4 dark:border-VerdeC border-MoradoO hover:bg-MoradoC dark:hover:bg-MoradoC focus-within:bg-MoradoO "
               >
                 <div className="flex rounded-md dark:border dark:border-VerdeO w-full h-full px-3 py-2 text-white font-bold justify-center">
